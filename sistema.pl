be27bf1d48d2c633c177b3e3446f1fc4719ee351
NicolasHrescak <131788252+NicolasHrescak@users.noreply.github.com>
2025-09-22 00:46:05 -0300

    Update sistema.pl
    Predicado calcula_pontuacao

--- sistema.pl
+++ sistema.pl
@@ -1,1 +1,10 @@
-
+% Predicado que serve para calcular a pontuação
+calcula_pontuacao(Trilha, Pontuacao, Total) :-
+    findall(Peso, perfil(Trilha, _, Peso), ListaPesos),% todos os pesos da trilha
+    sum_list(ListaPesos, Total), % soma total possível
+    findall(P, (
+        perfil(Trilha, Caracteristica, P),
+        pergunta(ID, _, Caracteristica),
+        resposta(ID, s)         % só conta se respondeu sim
+    ), ListaRespostas),
+    sum_list(ListaRespostas, Pontuacao).% soma apenas os pontos obtidos
