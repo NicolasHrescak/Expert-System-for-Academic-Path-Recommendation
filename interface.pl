4da90b70a76ffdac8e6e41177cfc5baf4e3b6b74
NicolasHrescak <131788252+NicolasHrescak@users.noreply.github.com>
2025-09-28 14:00:28 -0300

    Otimizando para a entrada direta no diretório correto.
    Dentro deste arquivo contém todo o sistema que chama a interface, nota-se que na primeira linha possui um diretório para a pasta onde estão todos os arquivos, esse diretório é a única linha que deve ser alterada ao rodar o código em outra máquina.

--- interface.pl
+++ interface.pl
@@ -1,1 +1,23 @@
 
+:- cd('c:/Users/vagner/Desktop/trabalho_prolog'). %Unica parte que se deve mudar ao rodar em outra maquina
+:- consult('base_conhecimento.pl'). %consulta automaticamente a base de conhecimento
+:- consult('sistema.pl'). %carrega todos os predicados pro funcionamento do sistema.
+
+:- initialization(main). %inicializa o predicado main que roda todo o trabalho
+
+main :-
+    writeln("Digite 1, 2 ou 3 para usar um perfil de teste pronto."),
+    writeln("Ou digite qualquer outra coisa para responder manualmente."),
+    flush_output,
+    read_line_to_string(user_input, Input),
+    (   Input = "1" -> usar_perfil('perfil1.pl') %utiliza o input como forma de verificar o arquivo correto
+    ;   Input = "2" -> usar_perfil('perfil2.pl')
+    ;   Input = "3" -> usar_perfil('perfil3.pl')
+    ;   iniciar
+    ).
+
+usar_perfil(Arquivo) :-
+    limpa_respostas,
+    consult(Arquivo),
+    recomenda(Resultado),
+    exibe_resultado(Resultado).
