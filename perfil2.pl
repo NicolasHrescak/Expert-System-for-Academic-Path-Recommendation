e9957d878deb0b1f5d360b051107383b4be11e40
NicolasHrescak <131788252+NicolasHrescak@users.noreply.github.com>
2025-09-28 14:04:12 -0300

    Perfil com afinidade em desenvolvimento web
    Perfil com respostas pré-definidas.

---
+++ perfil2.pl
@@ -0,0 +1,27 @@
+%PERFIL AFINIDADE COM DESENVOLVIMENTO WEB
+
+resposta(1, n).
+resposta(2, s).
+resposta(3, s).
+resposta(4, n).
+resposta(5, n).
+resposta(6, s).
+resposta(7, s).
+resposta(8, s).
+resposta(9, n).
+resposta(10, n).
+resposta(11, n).
+resposta(12, s).
+resposta(13, s).
+resposta(14, n).
+resposta(15, n).
+resposta(16, s).
+resposta(17, s).
+resposta(18, n).
+resposta(19, n).
+resposta(20, s).
+resposta(21, n).
+resposta(22, n).
+resposta(23, s).
+resposta(24, s).
+resposta(25, s).
