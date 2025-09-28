00896bc863a5f3f62e6907f73de2ee68a62d9d8d
NicolasHrescak <131788252+NicolasHrescak@users.noreply.github.com>
2025-09-28 14:03:44 -0300

    Perfil com afinidade para inteligência artificial.
    Perfil com respostas pré-definidas.

---
+++ perfil1.pl
@@ -0,0 +1,27 @@
+%PERFIL AFINIDADE COM IA 
+
+resposta(1, s).
+resposta(2, n).
+resposta(3, s).
+resposta(4, s).
+resposta(5, n).
+resposta(6, s).
+resposta(7, n).
+resposta(8, s).
+resposta(9, s).
+resposta(10, n).
+resposta(11, s).
+resposta(12, n).
+resposta(13, s).
+resposta(14, s).
+resposta(15, n).
+resposta(16, s).
+resposta(17, n).
+resposta(18, s).
+resposta(19, s).
+resposta(20, s).
+resposta(21, n).
+resposta(22, s).
+resposta(23, s).
+resposta(24, s).
+resposta(25, n).
