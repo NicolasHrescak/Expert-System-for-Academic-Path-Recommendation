e5f7c5dd09085118734e55534dfa13fb69999a3c
NicolasHrescak <131788252+NicolasHrescak@users.noreply.github.com>
2025-09-28 14:04:36 -0300

    Perfil com afinidade em redes.
    Perfil com respostas pré-definidas.

---
+++ perfil3.pl
@@ -0,0 +1,27 @@
+%PERFIL COM AFINIDADE EM REDES 
+
+resposta(1, s).
+resposta(2, n).
+resposta(3, s).
+resposta(4, s).
+resposta(5, s).
+resposta(6, s).
+resposta(7, n).
+resposta(8, s).
+resposta(9, s).
+resposta(10, s).
+resposta(11, n).
+resposta(12, n).
+resposta(13, s).
+resposta(14, n).
+resposta(15, s).
+resposta(16, s).
+resposta(17, n).
+resposta(18, s).
+resposta(19, n).
+resposta(20, s).
+resposta(21, s).
+resposta(22, s).
+resposta(23, s).
+resposta(24, s).
+resposta(25, s).
